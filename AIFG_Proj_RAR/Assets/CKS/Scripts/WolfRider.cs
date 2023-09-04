171452197beae742e6d2b7bdf89a5ffb701ec72d
C1l2j3-GH <chiakahsheng@gmail.com>
2023-09-04 14:58:55 +0800

    Update 04/09/2023
    1) Added 5 Scripts: AirBlade, Character_AirBladeAttack, WolfRider, IFMelee, and IFRanged
    2) Added 2 Prefabs for WolfRider and AirBlade
    3) Fixed bugs

---
+++ AIFG_Proj_RAR/Assets/CKS/Scripts/WolfRider.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfRider : MonoBehaviour, IFEnemy
+{
+    [Header("Preset")]
+    [Tooltip("Preset")]
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _altar;
+
+    [Header("Config")]
+    [Tooltip("Config")]
+    [SerializeField] private float _moveSpeed = 2f;
+    [SerializeField] private float _ragedMoveSpd = 10f;
+    [SerializeField] private float _rotateSpeed = 10f;
+    [SerializeField] private float _attackRange = 10f;
+    [SerializeField] private float _attackCD = 2f;
+
+    [Header("Debug")]
+    [SerializeField] private Rigidbody _wolfRiderRb;
+    [SerializeField] private Vector3 _altarDir;
+    [SerializeField] private Vector3 _rotateAng;
+    [SerializeField] private float _altarDistance;
+    [SerializeField] private bool _isAttacking;
+    [SerializeField] private bool _isFirstAttacked;
+    [SerializeField] private Coroutine _atkCoroutine;
+
+    public static bool _isPatroling;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _wolfRiderRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _altarDistance = Objects_AI.CalculateDistance(_altar.position, transform.position);
+
+        if(_isAttacking)
+        {
+            _atkCoroutine = StartCoroutine(StartAttack(_attackCD));
+        }
+    }
+    void FixedUpdate()
+    {
+        if (_altarDistance > _attackRange)
+        {
+            _altarDir = Objects_AI.CalculateDir(_altar.position, transform.position);
+            _rotateAng = Objects_AI.CalRotateAmount(_altarDir, transform.forward);
+            _wolfRiderRb.angularVelocity = Objects_AI.AimTarget(_wolfRiderRb, _rotateAng, _rotateSpeed);
+            _wolfRiderRb.velocity = new Vector3(_altarDir.x * _moveSpeed, 0, _altarDir.z * _moveSpeed);
+            StopAttackCoroutine();
+        }
+        else
+        {
+            _isAttacking = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+            if (!_isFirstAttacked)
+            {
+                _moveSpeed = _ragedMoveSpd;
+                _isFirstAttacked = true;
+            }
+            //Slow player and repel his first attack
+        }
+    }
+    private IEnumerator StartAttack(float cd)
+    {
+        _isAttacking = false;
+        //Enable attack collider and animation
+        //Straight away reduce Altar hp by time without collider
+
+        Debug.Log("WolfRider Attack!");
+        yield return new WaitForSeconds(cd);
+    }
+    private void StopAttackCoroutine()
+    {
+        if (_atkCoroutine != null)
+        {
+            StopCoroutine(_atkCoroutine);
+            _atkCoroutine = null;
+        }
+    }
+}
