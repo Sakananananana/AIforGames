112e17f2909ee89958a4b7c007d177ba1e8e3eaa
Hkchin <31932127+Hkchin@users.noreply.github.com>
2023-09-19 02:28:24 +0800

    fairy

--- AIFG_Proj_RAR/Assets/Script/AnimalWander.cs
+++ AIFG_Proj_RAR/Assets/Script/AnimalWander.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class AnimalWander : MonoBehaviour
 {
-     public Rigidbody rb;
-    public float maxSpeed = 6;
+    public Rigidbody rb;
+    public float maxSpeed = 0;
     public GameObject WanderAi;
     Vector3 targetPos;
     Vector3 targetDir;
     public int force;
-
-    Vector3 wamderDir;
-    float timer = 3f;
+    public GameObject healingMeat;
 
     public float angularVel = 1;
     public float accelRate;
@@ -29,38 +27,74 @@ public class AnimalWander : MonoBehaviour
 
     Vector3 circlePos;
     Vector3 circleCircumPos;
-    public float circleDist = 2f;
+    public float circleDist = 0;
     public float circleRad = 1f;
+
+    public bool isDead;
+    public bool isWalking;
+ 
+     public float timeSinceLastDirectionChange;
+     private float changeDirectionInterval = 5.0f;
     // Start is called before the first frame update
      void Start()
     {
         rb = GetComponent<Rigidbody>();
         accelRate = maxSpeed / accelTimeToMax;
-
+        isDead = false;
+        isWalking = false;
     }
-
-
 
 
 
     // Update is called once per frame
     void Update()
     {
-
-        rb.AddForce(transform.up * gravity * Time.deltaTime);
-        circlePos = WanderAi.transform.position + ((curDir * currentSpeed).normalized) * circleDist;
+        
         
 
+        //wander part
+        timeSinceLastDirectionChange += Time.deltaTime;
+          if(timeSinceLastDirectionChange >= changeDirectionInterval) 
+        {
+       
+        
+        
 
+        //set direction
         var vector2 = Random.insideUnitCircle.normalized * circleRad;
+        //change direction if near altar
+        if(vector2.y < 15 || vector2.y >-5 || vector2.x < 10 || vector2.x > -10 )
+        {
+
+            vector2 = Random.insideUnitCircle.normalized * circleRad;
+        }
+
+
         circleCircumPos = new Vector3(vector2.x,0,vector2.y);
         targetPos =circlePos + circleCircumPos;
-        
+
+         
         targetDir = (targetPos - WanderAi.transform.position).normalized;
         targetDir.y = 0;
+        timeSinceLastDirectionChange = 0.0f;
+       }
+ 
+        //Debug.Log(targetDir);
+        rb.AddForce(transform.up * gravity * Time.deltaTime);
+        circlePos = WanderAi.transform.position + ((curDir * currentSpeed).normalized) * circleDist;
+        Debug.Log(isWalking);
+        if(isWalking == false)
+        {
+        StartCoroutine(WanderAround());
+isWalking = false;
 
-        curDir = Vector3.RotateTowards(WanderAi.transform.forward,targetDir, 3*Time.deltaTime, 0.0f);
+        }
+        curDir = Vector3.RotateTowards(WanderAi.transform.forward,targetDir, 5*Time.deltaTime, 0.0f);
+        //Debug.Log(curDir);
         WanderAi.transform.rotation = Quaternion.LookRotation(curDir);
+
+
+       
 
         if(currentSpeed < maxSpeed)
         {
@@ -75,12 +109,52 @@ public class AnimalWander : MonoBehaviour
 
         steerDir = targetDir - curDir;
         rb.velocity = (currentSpeed * curDir) + (steerDir *steerForce);
+        // end wandering
 
+        
+        if(isDead == true)
+        {
+
+            amDead();
+        }
 
         Debug.DrawRay(WanderAi.transform.position, targetDir, Color.black);
         Debug.DrawRay(WanderAi.transform.position, WanderAi.transform.forward, Color.green);
         Debug.DrawRay(WanderAi.transform.position, steerDir, Color.red);
 
     }
+    
+    IEnumerator WanderAround()
+    {
+        //wandering movement
+        
+        isWalking = true;
+        yield return new WaitForSeconds(3f);    
+        
+        
+        maxSpeed = 6;
+        isWalking = false;
+                
+       
+       
+
+    }
+    
+   
+ void amDead()
+ {
+    Destroy(gameObject);
+    Instantiate(healingMeat, WanderAi.transform.position, Quaternion.identity);
+
+ }
+
+
+      private void OnTriggerEnter(Collider other)
+      {
+        if (other.CompareTag("Sword"))
+        {
+        isDead = true;
+        }
+      }
 
 }
---
+++ AIFG_Proj_RAR/Assets/Script/FairyFollow.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyFollow : MonoBehaviour
+{
+   public Rigidbody rb;
+    private float maxSpeed = 4;
+    public GameObject player;
+    public GameObject SeekAi;
+    Vector3 targetPos;
+    Vector3 targetDir;
+    public int force;
+
+    public float angularVel = 1;
+    public float accelRate;
+    public float accelTimeToMax = 3f;
+    public float currentSpeed;
+    Vector3 steerDir;
+    Vector3 curDir;
+
+    public float steerForce = 1f;
+    private float gravity = -9.8f;
+    PlayerMovements playerMVM;
+    int distance = 5;
+    bool fairyCorout;
+    // Start is called before the first frame update
+   void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerMVM = gameObject.GetComponent<PlayerMovements>();
+        accelRate = maxSpeed / accelTimeToMax;
+        fairyCorout = true;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fairyCorout == true)
+        {
+        StartCoroutine(fairyActions());
+        }
+        rb.AddForce(transform.up * gravity * Time.deltaTime);
+        targetPos = player.transform.position;
+        targetDir = (targetPos - SeekAi.transform.position).normalized;
+        targetDir.y = 0;
+
+        curDir = Vector3.RotateTowards(SeekAi.transform.forward,targetDir, 3*Time.deltaTime, 0.0f);
+        SeekAi.transform.rotation = Quaternion.LookRotation(curDir);
+
+
+
+    if(Vector3.Distance(SeekAi.transform.position, player.transform.position)<=distance)
+    {
+        currentSpeed = 0;
+    }
+    else
+    {
+        {
+        if(currentSpeed < maxSpeed)
+        {
+
+            currentSpeed = currentSpeed + accelRate * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed = maxSpeed;
+
+        }
+        steerDir = targetDir - curDir;
+        rb.velocity = (currentSpeed * curDir) + (steerDir *steerForce);
+
+    }
+}
+        
+
+        Debug.DrawRay(SeekAi.transform.position, targetDir, Color.black);
+        Debug.DrawRay(SeekAi.transform.position, SeekAi.transform.forward, Color.green);
+        Debug.DrawRay(SeekAi.transform.position, steerDir, Color.red);
+
+    }
+
+
+    IEnumerator fairyActions()
+    {
+        
+        
+            int actionNo = (Random.Range(0,12))%3;
+
+            if (actionNo == 0)
+            {
+
+             //playerMVM.HP += 10;
+               
+            } 
+
+            if(actionNo == 1)
+            {
+
+             // playerMVM.speed +=4f;
+               
+            }
+
+            if (actionNo == 2)
+            {
+
+             // playerMVM.damage *=2;
+               
+
+            }
+            fairyCorout = false;
+        
+        yield return new WaitForSeconds(10f);
+        fairyCorout = true;
+
+    }
+}
